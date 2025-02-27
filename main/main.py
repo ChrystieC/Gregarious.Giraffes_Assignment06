ad88a4d2f6bdfb065bb49bae969eac22ffddbbf0
Richie james <rjxl17@gmail.com>
2025-02-26 19:38:43 -0500

    Added my portion on the richie folder

--- main/main.py
+++ main/main.py
@@ -1,5 +1,14 @@
 # main.py
 # IS4010-001 Boilerplate Project
 
+#main.py
+
 if __name__ == "__main__":
-    pass
+    ucBuilding = object("Crosley", 16 + "Stories",)
+    nippert_stadium = NippertStadium("Nippert Stadium", 1924, 40000, 5, "Cincinnati Bearcats")
+
+
+    print(ucBuilding)
+    print(nippert_stadium)
+
+    ucBuilding.add_classroom()
